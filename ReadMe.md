3d6df5d2888cce6f62a6df58a62251201d881e97
gentilpedro <gentil.pedro21@gmail.com>
2025-03-28 11:10:41 -0300

    Feito ajustes de metodos padrão e config do prisma

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+id, destino, transporte (“Terrestre”, “Marítimo” ou “Aéreo”) dataSaida (valor
+default deve ser a data atual - now()), preco e duracao (em dias).
+Ajustar para que o nome da tabela fique viagens.
+Criar e armazenar os dados no banco turismo_nome_aluno
+
+
+tem que criar o banco de dados turismo_nome_aluno e criar a tabela viagens 
+
+nao esquecer de trocar no arquivo .env o nome do banco de dados e o nome do usuario e senha para o banco de dados
--- routes/vaigens.ts
+++ routes/vaigens.ts
@@ -1,6 +1,69 @@
 import { Router } from "express";
 import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient();
+const router = Router();
 
 
-export default Router;
+
+router.get("/", async (req, res) => {
+    const viagens = await prisma.viagen.findMany()
+    res.status(200).json(viagens)
+})
+
+router.post("/", async (req, res) => {
+    const { titulo, genero, duracao, preco, sinopse } = req.body
+
+    if (!titulo || !genero || !duracao || !preco) {
+        res.status(400).json({ erro: "Informe todos os dados" })
+        return
+    }
+
+    const viagen = await prisma.viagen.create({
+        data: { titulo, genero, duracao, preco, sinopse }
+    })
+
+    res.status(201).json(viagen)
+})
+
+router.put("/:id", async (req, res) => {
+    // recebe o id passado como parâmetro
+    const { id } = req.params
+
+    // recebe as variáveis vindas no corpo da requisição
+    const { titulo, genero, duracao, preco, sinopse } = req.body
+
+    // verifica se os campos obrigatórios foram passados
+    if (!titulo || !genero || !duracao || !preco) {
+        res.status(400).json({ erro: "Informe todos os dados" })
+        return
+    }
+
+    try {
+        const viagen = await prisma.viagen.update({
+            where: { id: Number(id) },
+            data: { titulo, genero, duracao, preco, sinopse }
+        })
+        res.status(200).json(viagen)
+    } catch (error) {
+        res.status(400).json({ erro: error })
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    // recebe o id passado como parâmetro
+    const { id } = req.params
+
+    // realiza a exclusão do viagen
+    try {
+        const viagen = await prisma.viagen.delete({
+            where: { id: Number(id) }
+        })
+        res.status(200).json(viagen)
+    } catch (error) {
+        res.status(400).json({ erro: error })
+    }
+})
+
+
+export default router;  
